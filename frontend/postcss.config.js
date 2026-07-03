27bc9b8a90cd183996c644c6393d77cb7b03b440
Nguyễn Thuận <thuantokutou@gmail.com>
2026-07-03 16:03:04 +0700

    chore(frontend): migrate app from Vite to CRA

--- frontend/postcss.config.js
+++
@@ -1,7 +0,0 @@
-// TODO: Configure PostCSS plugins used by Tailwind CSS.
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// TODO: Configure Tailwind content paths and theme tokens later.
-export default {
-  content: ["./index.html", "./src/**/*.{js,css}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
